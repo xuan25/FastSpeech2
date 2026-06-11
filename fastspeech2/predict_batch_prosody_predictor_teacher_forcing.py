c92031d38ed2c41fea877fd9f7f3fdd568f4baab
Xuan25 <xuan@xuan25.com>
2026-06-11 11:31:13 +0000

    pred_teacher_forcing

---
+++ fastspeech2/predict_batch_prosody_predictor_teacher_forcing.py
@@ -0,0 +1,230 @@
+import argparse
+import csv
+import functools
+import os
+
+import numpy as np
+import torch
+import tqdm
+from torch.utils.data import DataLoader
+
+from .model.data_models import ProsodyPredictorOutput
+from .model.prosody_predictor import ProsodyPredictor
+
+from .dataset.datasetfs import DatasetFS
+
+from .config import (
+    DatasetConfig, 
+    DatasetFeaturePropertiesConfig, 
+    ModelConfig, 
+)
+from .dataset.data_models import DataBatch, DataBatchTorch, DatasetFeatureStats
+from .dataset.dataset import DatasetSplit, DatasetWithLabel, TextOnlyDatasetWithLabel
+
+torch.serialization.add_safe_globals([
+    np._core.multiarray.scalar, 
+    np.dtype, np.dtypes.Float64DType, 
+    # DatasetConfig, DatasetPathConfig, 
+    # DatasetFeaturePropertiesConfig, 
+    # DatasetPreprocessingConfig, 
+    # ModelConfig, 
+    # ModelTransformerConfig,
+    # ModelVariancePredictorConfig,
+    # ModelVarianceEmbeddingConfig,
+    # ModelVocoderConfig,
+    # ModelGlobalConfig,
+    # TrainConfig,
+    # TrainOutputConfig,
+    # TrainOptimizerConfig,
+    # TrainStepConfig,
+    # LossConfig,
+    # DatasetFeatureStats
+    ])
+
+def get_model_infer(ckpt_path, 
+              model_config: ModelConfig,
+              dataset_feature_properties_config: DatasetFeaturePropertiesConfig,
+              dataset_feature_stats: DatasetFeatureStats, 
+              device) -> ProsodyPredictor:
+
+    model = ProsodyPredictor(model_config, dataset_feature_properties_config, dataset_feature_stats).to(device)
+    if ckpt_path:
+        ckpt = torch.load(ckpt_path, map_location=device)
+        model.load_state_dict(ckpt["model"])
+
+    model.eval()
+    # model.requires_grad_ = False
+    model.requires_grad_(False)
+    return model
+
+dataset_cache = {}
+def get_dataset(dataset_config: DatasetConfig, data_split: DatasetSplit) -> DatasetWithLabel:
+    key = (dataset_config, data_split)
+    if key in dataset_cache:
+        dataset = dataset_cache[key]
+        if dataset.get_dataset_fs().tar is not None:
+            return dataset
+    
+    dataset = DatasetWithLabel(
+        dataset_config.path_config,
+        dataset_config.preprocessing_config,
+        data_split
+    )
+    dataset_cache[key] = dataset
+    return dataset
+
+def process(ckpt_path: str, output_path: str, dataset_config_path: str, model_config_path: str, data_split_name: str, pitch_control: float, energy_control: float, duration_control: float, batch_size: int, label_control: int):
+    control_values = pitch_control, energy_control, duration_control
+    dataset_config = DatasetConfig.load_from_yaml(dataset_config_path)
+    model_config = ModelConfig.load_from_yaml(model_config_path)
+    data_split = DatasetSplit[data_split_name.upper()]
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Get dataset
+    dataset = get_dataset(dataset_config, data_split)
+    dataset_fs = dataset.get_dataset_fs()
+
+    with dataset_fs.open(dataset_config.path_config.stats_file) as stats_stream, \
+        dataset_fs.open(dataset_config.path_config.speaker_map_file) as speaker_stream:
+        # Load dataset feature statistics
+        dataset_feature_stats = DatasetFeatureStats.from_json(
+            stats_stream,
+            speaker_stream,
+        )
+
+    # Get model
+    model: ProsodyPredictor = get_model_infer(ckpt_path, model_config, dataset_config.feature_properties_config, dataset_feature_stats, device)
+
+    batchs = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=0,
+        collate_fn=dataset.collate_fn,
+    )
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    pitch_control, energy_control, duration_control = control_values
+
+    with open(output_path, "w", encoding="utf-8", newline="") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["data_id", "phone_idx", "phone", "pitch", "energy", "duration", "label", "label_source"])
+
+        for batch in tqdm.tqdm(batchs, desc="[Decoding]", dynamic_ncols=True, leave=False):
+            batch: DataBatch = batch
+
+            if label_control >= 0:
+                for i, sample in enumerate(batch):
+                    sample.label = label_control
+                assert batch.labels is not None
+                batch.labels = np.array([label_control] * batch.labels.shape[0], dtype=np.int64)
+
+            batch_torch: DataBatchTorch = batch.to_torch(device)
+            with torch.no_grad():
+                # Forward
+                output: ProsodyPredictorOutput = model(
+                    batch_torch,
+                    p_control=pitch_control,
+                    e_control=energy_control,
+                    d_control=duration_control
+                )
+
+                for i, sample in enumerate(batch):
+                    sample_id = sample.data_id
+                    for j, phone in enumerate(sample.text):
+                        pitch = output.pitch_predictions[i, j].item()
+                        energy = output.energy_predictions[i, j].item()
+                        duration_log = output.log_duration_predictions[i, j]
+                        duration = torch.exp(duration_log).item()  # Convert log duration to actual duration
+                        from .text.symbols import symbols
+                        phone_alphabet = symbols[phone]
+                        csv_writer.writerow([sample_id, j, phone_alphabet, pitch, energy, duration, sample.label, sample.label_source])
+
+    # dataset_fs.close()
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ckpt_path", 
+        type=str, 
+        default="output/default/ckpt/4000.pth"
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="output/default/synth_val",
+        help="path to a source file with format like LibriTTS dataset",
+    )
+    parser.add_argument(
+        "-d",
+        "--dataset_config",
+        type=str,
+        # required=True, 
+        default="config/LibriTTS/dataset.yaml",
+        help="path to dataset.yaml",
+    )
+    parser.add_argument(
+        "-m", "--model_config", 
+        type=str, 
+        # required=True, 
+        help="path to model.yaml",
+        default="config/LibriTTS/model.yaml",
+    )
+    parser.add_argument(
+        "--data_split",
+        type=str,
+        default="val",
+        choices=["train", "val"],
+        help="data split to use for synthesis",
+    )
+    parser.add_argument(
+        "--pitch_control",
+        type=float,
+        default=1.0,
+        help="control the pitch of the whole utterance, larger value for higher pitch",
+    )
+    parser.add_argument(
+        "--energy_control",
+        type=float,
+        default=1.0,
+        help="control the energy of the whole utterance, larger value for larger volume",
+    )
+    parser.add_argument(
+        "--duration_control",
+        type=float,
+        default=1.0,
+        help="control the speed of the whole utterance, larger value for slower speaking rate",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help="batch size for synthesis",
+    )
+    parser.add_argument(
+        "--label_control",
+        type=int,
+        default=-1,
+        help="control the label of the whole utterance, -1 for no control",
+    )
+    args = parser.parse_args()
+
+    process(
+        ckpt_path=args.ckpt_path,
+        output_path=args.output_path,
+        dataset_config_path=args.dataset_config,
+        model_config_path=args.model_config,
+        data_split_name=args.data_split,
+        pitch_control=args.pitch_control,
+        energy_control=args.energy_control,
+        duration_control=args.duration_control,
+        batch_size=args.batch_size,
+        label_control=args.label_control,
+    )
+
+if __name__ == "__main__":
+    main()
+
+    print("done!")
---
+++ prosody_predictor_predict_batch_label_teacher_forcing.py
@@ -0,0 +1,88 @@
+import argparse
+import csv
+import tqdm
+from fastspeech2.predict_batch_prosody_predictor_teacher_forcing import process
+
+def get_labels_from_index_file(label_index_file):
+    with open(label_index_file, 'r') as f:
+        reader = csv.DictReader(f)
+        assert reader.fieldnames is not None, "CSV file must have header"
+        labels = reader.fieldnames[1:]  # Skip the first column which is assumed to be 'basename' or similar
+    return labels
+
+if __name__ == "__main__":
+
+    arg_parser = argparse.ArgumentParser(
+        description="Predict prosody features using a trained prosody predictor model."
+    )
+    arg_parser.add_argument(
+        "--model_ckpt_dir", type=str, required=True, help="Model checkpoint directory. e.g. output/dataset/label/model/variant/loss"
+    )
+    arg_parser.add_argument(
+        "--dataset_config_path", type=str, required=True, help="Dataset configuration file path. e.g. config/dataset.yaml"
+    )
+    arg_parser.add_argument(
+        "--model_config_path", type=str, help="Model configuration file path. e.g. config/model.yaml"
+    )
+    arg_parser.add_argument(
+        "--label_index_file", type=str, required=True, help="Label index CSV file. e.g. data/label.csv"
+    )
+    arg_parser.add_argument(
+        "--data_split_name", type=str, default="train", help="Data split name to process. e.g. train or val"
+    )
+    arg_parser.add_argument(
+        "--ckpt_begin", type=int, default=1000, help="Start checkpoint number to process. e.g. 1000"
+    )
+    arg_parser.add_argument(
+        "--ckpt_end", type=int, default=400000, help="End checkpoint number to process. e.g. 400000"
+    )
+    arg_parser.add_argument(
+        "--ckpt_step", type=int, default=40000, help="Step size between checkpoints to process. e.g. 40000"
+    )
+    arg_parser.add_argument(
+        "--skip_conf_steps", type=int, default=0, help="Number of initial configurations to skip. Useful for resuming after a failure. e.g. 0"
+    )
+    args = arg_parser.parse_args()
+
+
+
+    label_names = get_labels_from_index_file(args.label_index_file)
+
+    configs = [
+        {
+            "ckpt_path": f"{args.model_ckpt_dir}/ckpt/{model_ckpt_num}.pth",
+            "output_path": f"{args.model_ckpt_dir}/pred_teacher_forcing/{args.data_split_name}/{label_name}/{model_ckpt_num}.csv",
+            "dataset_config_path":args.dataset_config_path,
+            "model_config_path": args.model_config_path,
+            "data_split_name": args.data_split_name,
+            "pitch_control": 1.0,
+            "energy_control": 1.0,
+            "duration_control": 1.0,
+            "batch_size": 16,
+            "label_control": label_control,
+        }
+        for model_ckpt_num in range(args.ckpt_begin, args.ckpt_end + 1, args.ckpt_step)
+        for label_control, label_name in [
+            (-1, "default"),
+        ] + [(i, label_names[i]) for i in range(len(label_names))]
+    ]
+
+    left_to_skip = args.skip_conf_steps
+
+    success_count = 0
+    for config in tqdm.tqdm(configs, desc="Processing configurations", dynamic_ncols=True, leave=False, miniters=1, mininterval=0):
+        if left_to_skip > 0:
+            left_to_skip -= 1
+            tqdm.tqdm.write(f"Skipping configuration")
+            continue
+        try:
+            process(**config)
+            success_count += 1
+        except Exception as e:
+            tqdm.tqdm.write(f"Failed to process: {config}")
+            tqdm.tqdm.write(str(e))
+        
+    if success_count == len(configs):
+        print("Processing completed for all configurations.")
+    else:
+        print(f"Processing completed for {success_count} of {len(configs)} configurations.")
